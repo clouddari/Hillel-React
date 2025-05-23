771ce652581d7f77f751583097bb3be9d26c3560
Dariia K <166187273+clouddari@users.noreply.github.com>
2025-05-23 11:26:15 +0700

    hw42 calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+const numbers = Array.from({ length: 10 }, (_, i) => i);
+console.log(numbers);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [clickedNumbers, setClickedNumbers] = useState([]);
+  const [result, setResult] = useState(null);
+
+  const handleClick = (num) => {
+    setClickedNumbers((prev) => {
+      if (prev.length > 0 && prev[prev.length - 1] === "*" && num === "*") {
+        return [...prev.slice(0, -1), "**"];
+      }
+      return [...prev, num];
+    });
+  };
+
+  const getTheResultOfBasicOperations = (a, b, operation) => {
+    switch (operation) {
+      case "+":
+        return a + b;
+      case "-":
+        return a - b;
+
+      case "/":
+        if (b === 0) {
+          return "Error: Division by zero";
+        }
+        return a / b;
+
+      case "*":
+        return a * b;
+
+      case "**":
+        return a ** b;
+
+      case "%":
+        return (a * b) / 100;
+
+      case "√":
+        return Math.sqrt(a);
+
+      default:
+        return null;
+    }
+  };
+
+  const calculateResult = () => {
+    const operators = ["+", "-", "*", "/", "**", "√", "%"];
+    const opIndex = clickedNumbers.findIndex((el) => operators.includes(el));
+    if (opIndex === -1) {
+      setResult("No operation found");
+      return;
+    }
+
+    const leftArr = clickedNumbers.slice(0, opIndex);
+    const rightArr = clickedNumbers.slice(opIndex + 1);
+
+    const leftNum = Number(leftArr.join(""));
+    const rightNum = Number(rightArr.join(""));
+    const operator = clickedNumbers[opIndex];
+
+    if (isNaN(leftNum) || isNaN(rightNum)) {
+      setResult("Invalid input");
+      return;
+    }
+
+    const res = getTheResultOfBasicOperations(leftNum, rightNum, operator);
+    setResult(res);
+  };
+
+  const clearResult = () => {
+    setClickedNumbers([]);
+    setResult(null);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <h2>Calculator HW42</h2>
+      </header>
+
+      <div className="operations">
+        <p>{clickedNumbers.join("")} </p>
+        <h3>{result !== null ? result : ""}</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="buttons">
+        <div className="numbers">
+          {numbers.map((number) => (
+            <button
+              onClick={() => {
+                handleClick(number);
+              }}
+              key={number}
+            >
+              {number}
+            </button>
+          ))}
+        </div>
+
+        <div className="operators">
+          {["+", "-", "/", "*"].map((op) => (
+            <button onClick={() => handleClick(op)} key={op}>
+              {op}
+            </button>
+          ))}
+
+          <button onClick={() => handleClick("√")} key="sqrt">
+            √
+          </button>
+          <button onClick={() => handleClick("**")} key="exponent">
+            (x)ʸ
+          </button>
+          <button onClick={() => handleClick("%")} key="percentage">
+            %
+          </button>
+          <button onClick={calculateResult} key="equals">
+            =
+          </button>
+
+          <button key="clear" onClick={clearResult}>
+            {" "}
+            C
+          </button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
